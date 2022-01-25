9048a92b33b4db87f5df645bd63d93f4106a9d60
Yannis Bouhamou <61419311+Yanka13@users.noreply.github.com>
2022-01-25 11:38:27 +0400

    Added sentry for monitoring on production and created a bug on purpose for error 500 /error

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,26 @@
 import random
 from flask import Flask, jsonify
 
+import os
+sentry_dsn = os.getenv('SENTRY_DSN', None)
+if sentry_dsn:
+    import sentry_sdk
+    from sentry_sdk.integrations.flask import FlaskIntegration
+    sentry_sdk.init(
+        sentry_dsn,
+        integrations=[FlaskIntegration()]
+    )
 
 app = Flask(__name__)
 
 
-# blabla
+# homepage
 @app.route('/')
 def home():
     return jsonify({ 'roll': random.randint(1,6) })
+
+#endpoint error
+@app.route('/error')
+def error():
+
+    return {1/0}
